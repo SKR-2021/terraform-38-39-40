70df40d2c5f8cdfa3960d48f6aa6b212fa941f0b
SKR-2021 <karunakar2000@gmail.com>
2026-07-20 12:41:43 +0200

    terraform-updated

--- dotmart_dev_infra/10-sg/locals.tf
+++ dotmart_dev_infra/10-sg/locals.tf
@@ -1,3 +1,4 @@
 locals {
     comman_name_suffix = "${var.project_name}-${var.environment}" # dotmart-dev
+    vpc_id = data.aws_ssm_parameter.vpc_id.value
 }
--- dotmart_dev_infra/10-sg/main.tf
+++ dotmart_dev_infra/10-sg/main.tf
@@ -1,11 +1,22 @@
-module "catalogue" {
-    source = "terraform-aws-modules/security-group/aws"
+# Open Source Module 
+# module "catalogue" {
+#     source = "terraform-aws-modules/security-group/aws"
 
 
-    name            = "${local.comman_name_suffix}-catalogue-sg"
-    use_name_prefix = false
-    description     = "Security Group for catalogue with custom ports"
-    vpc_id          =  data.aws_ssm_parameter.vpc_id.value 
+#     name            = "${local.comman_name_suffix}-catalogue-sg"
+#     use_name_prefix = false
+#     description     = "Security Group for catalogue with custom ports"
+#     vpc_id          =  data.aws_ssm_parameter.vpc_id.value 
+
+# }
+
+
+module "sg" {
+    source = "git::https://github.com/SKR-2021/terraform-38-39-40.git?ref=main//terraform_aws_sg"
+    project_name    =   var.project_name  
+    environment     =   var.environment
+    sg_name         =   "mongodb-sg"
+    sg_description  =   "Created-SG-for-mongodb"
+    vpc_id          =   local.vpc_id
 
 }
-
