d1e38fd9fde9f1da92dc8022b029148665041a67
SKR-2021 <karunakar2000@gmail.com>
2026-08-07 18:36:03 +0200

    bootstrap

--- dotmart_dev_infra/40-databases/main.tf
+++ dotmart_dev_infra/40-databases/main.tf
@@ -24,9 +24,16 @@ resource "terraform_data" "mongodb" {
     host        =    aws_instance.mongodb.private_ip
 }
 
+  provisioner "file" {
+    source = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+
+  }
+
   provisioner "remote-exec" {
     inline = [ 
-      "echo Hello World"
+        "chmod +x /tmp/bootstrap.sh",
+        "sudo sh /tmp/bootstrap.sh"
     ]
   }
 }
