a5206daf69b9d5b99753685bfd8f82254c39746b
SKR-2021 <karunakar2000@gmail.com>
2026-08-10 14:26:15 +0200

    rabbit&mysql

--- dotmart_dev_infra/40-databases/main.tf
+++ dotmart_dev_infra/40-databases/main.tf
@@ -56,7 +56,7 @@ resource "aws_instance" "redis" {
 
 resource "terraform_data" "redis" {
   triggers_replace = [
-    aws_instance.mongodb.id
+    aws_instance.redis.id
   ]
 
   connection {
@@ -76,6 +76,88 @@ resource "terraform_data" "redis" {
     inline = [ 
         "chmod +x /tmp/bootstrap.sh",
         "sudo sh /tmp/bootstrap.sh redis"
+    ]
+  }
+}
+
+# RabbitMQ Database
+resource "aws_instance" "rabbitmq" {
+  ami                    = local.ami_id
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [local.rabbitmq_sg_id]
+  subnet_id = local.database_subnet_ids
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project_name}-${var.environment}-rabbitmq-vm"
+    }
+  )
+}
+
+resource "terraform_data" "rabbitmq" {
+  triggers_replace = [
+    aws_instance.rabbitmq.id
+  ]
+
+  connection {
+    type        =   "ssh"
+    user        =   "ec2-user"
+    password    =   "DevOps321"
+    host        =    aws_instance.rabbitmq.private_ip
+}
+
+  provisioner "file" {
+    source = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+
+  }
+
+  provisioner "remote-exec" {
+    inline = [ 
+        "chmod +x /tmp/bootstrap.sh",
+        "sudo sh /tmp/bootstrap.sh rabbitmq"
+    ]
+  }
+}
+
+# Mysql Database
+resource "aws_instance" "mysql" {
+  ami                    = local.ami_id
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [local.mysql_sg_id]
+  subnet_id = local.database_subnet_ids
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project_name}-${var.environment}-mysql-vm"
+    }
+  )
+}
+
+resource "terraform_data" "mysql" {
+  triggers_replace = [
+    aws_instance.mysql.id
+  ]
+
+  connection {
+    type        =   "ssh"
+    user        =   "ec2-user"
+    password    =   "DevOps321"
+    host        =    aws_instance.mysql.private_ip
+}
+
+  provisioner "file" {
+    source = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+
+  }
+
+  provisioner "remote-exec" {
+    inline = [ 
+        "chmod +x /tmp/bootstrap.sh",
+        "sudo sh /tmp/bootstrap.sh mysql"
     ]
   }
 }
