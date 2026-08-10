5f4e40cdc08a0bb1f2a1a147631833f490ca0217
SKR-2021 <karunakar2000@gmail.com>
2026-08-10 14:06:31 +0200

    redis

--- dotmart_dev_infra/40-databases/bootstrap.sh
+++ dotmart_dev_infra/40-databases/bootstrap.sh
@@ -1,4 +1,5 @@
 #!bin/bash
-dnf install ansible -y
 
-sudo ansible-pull -U https://github.com/daws-86s/ansible-roboshop-roles-tf.git -e component=mongodb main.yaml
+component=$1
+dnf install ansible -y
+sudo ansible-pull -U https://github.com/daws-86s/ansible-roboshop-roles-tf.git -e component=$component main.yaml
