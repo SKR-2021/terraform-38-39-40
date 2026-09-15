7e006f352907417adc9d3009e14e7fc462800784
SKR-2021 <karunakar2000@gmail.com>
2026-09-15 17:23:05 +0200

    redis

--- dotmart_dev_infra/40-databases/bootstrap.sh
+++ dotmart_dev_infra/40-databases/bootstrap.sh
@@ -1,5 +1,5 @@
 #!bin/bash
 
-# component=$1
+component=$1
 dnf install ansible -y
-sudo ansible-pull -U https://github.com/SKR-2021/ansible-dotmart-roles-tf.git -e component=mongodb main.yaml
+sudo ansible-pull -U https://github.com/SKR-2021/ansible-dotmart-roles-tf.git -e component=$component main.yaml
