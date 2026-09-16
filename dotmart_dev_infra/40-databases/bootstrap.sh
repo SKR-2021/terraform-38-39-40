1a716dbeadd875898d0cc22a48a9c4b0045d55c6
SKR-2021 <karunakar2000@gmail.com>
2026-09-16 20:53:20 +0200

    mysql

--- dotmart_dev_infra/40-databases/bootstrap.sh
+++ dotmart_dev_infra/40-databases/bootstrap.sh
@@ -1,4 +1,27 @@
 #!bin/bash
 component=$1
+environment=$2
 dnf install ansible -y
-sudo ansible-pull -U https://github.com/SKR-2021/ansible-dotmart-roles-tf.git -e component=$component main.yaml
+# sudo ansible-pull -U https://github.com/SKR-2021/ansible-dotmart-roles-tf.git -e component=$component main.yaml
+
+REPO_URL=https://github.com/SKR-2021/ansible-dotmart-roles-tf.git
+REPO_DIR=/opt/roboshop/ansible
+ANSIBLE_DIR=ansible-dotmart-roles-tf
+
+mkdir -p $REPO_DIR
+mkdir -p /var/log/roboshop
+touch ansible.log
+
+cd $REPO_DIR
+
+
+if [ -d $ANSIBLE_DIR ]; then
+    cd $ANSIBLE_DIR
+    git pull
+
+else
+    git clone $REPO_URL
+    cd $ANSIBLE_DIR
+fi
+
+ansible-playbook -e component=$component environment=$environment main.yaml
--- dotmart_dev_infra/40-databases/main.tf
+++ dotmart_dev_infra/40-databases/main.tf
@@ -125,42 +125,48 @@ resource "terraform_data" "rabbitmq" {
 }
 
 # Mysql Database
-# resource "aws_instance" "mysql" {
-#   ami                    = local.ami_id
-#   instance_type          = "t3.micro"
-#   vpc_security_group_ids = [local.mysql_sg_id]
-#   subnet_id = local.database_subnet_ids
+resource "aws_instance" "mysql" {
+  ami                    = local.ami_id
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [local.mysql_sg_id]
+  subnet_id = local.database_subnet_ids
+  iam_instance_profile = aws_iam_instance_profile.mysql.name
 
-#   tags = merge(
-#     local.common_tags,
-#     {
-#       Name = "${var.project_name}-${var.environment}-mysql-vm"
-#     }
-#   )
-# }
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project_name}-${var.environment}-mysql-vm"
+    }
+  )
+}
 
-# resource "terraform_data" "mysql" {
-#   triggers_replace = [
-#     aws_instance.mysql.id
-#   ]
+resource "aws_iam_instance_profile" "mysql" {
+  name = "mysql"
+  role = "EC2SSMParametersPolicy"
+}
 
-#   connection {
-#     type        =   "ssh"
-#     user        =   "ec2-user"
-#     password    =   "DevOps321"
-#     host        =    aws_instance.mysql.private_ip
-# }
+resource "terraform_data" "mysql" {
+  triggers_replace = [
+    aws_instance.mysql.id
+  ]
 
-#   provisioner "file" {
-#     source = "bootstrap.sh"
-#     destination = "/tmp/bootstrap.sh"
+  connection {
+    type        =   "ssh"
+    user        =   "ec2-user"
+    password    =   "DevOps321"
+    host        =    aws_instance.mysql.private_ip
+}
 
-#   }
+  provisioner "file" {
+    source = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
 
-#   provisioner "remote-exec" {
-#     inline = [ 
-#         "chmod +x /tmp/bootstrap.sh",
-#         "sudo sh /tmp/bootstrap.sh mysql"
-#     ]
-#   }
-# }
+  }
+
+  provisioner "remote-exec" {
+    inline = [ 
+        "chmod +x /tmp/bootstrap.sh",
+        "sudo sh /tmp/bootstrap.sh mysql dev"
+    ]
+  }
+}
