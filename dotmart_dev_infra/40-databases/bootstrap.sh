5bba7a9d03aa01853da655cbd624ec6c502ef620
SKR-2021 <karunakar2000@gmail.com>
2026-09-16 22:03:16 +0200

    updated

--- dotmart_dev_infra/40-databases/bootstrap.sh
+++ dotmart_dev_infra/40-databases/bootstrap.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 component=$1
-environment=$2
+env=$2
 dnf install ansible -y
 # sudo ansible-pull -U https://github.com/SKR-2021/ansible-dotmart-roles-tf.git -e component=$component main.yaml
 
@@ -24,4 +24,4 @@ else
     cd $ANSIBLE_DIR
 fi
 
-ansible-playbook -e component=$component -e environment=$environment main.yaml
+ansible-playbook -e component=$component -e env=$env main.yaml
