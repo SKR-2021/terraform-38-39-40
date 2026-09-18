d21012773a21245ad9481801faa93032220d2f8f
SKR-2021 <karunakar2000@gmail.com>
2026-09-18 13:06:46 +0200

    catalogue2

--- dotmart_dev_infra/40-databases/main.tf
+++ dotmart_dev_infra/40-databases/main.tf
@@ -171,39 +171,39 @@ resource "terraform_data" "mysql" {
   }
 }
 
-  resource "aws_route53_record" "mongodb" {
-    zone_id = data.aws_route53_zone.main.zone_id
-    name    = "mongodb-${var.environment}.${var.domain_name}" # mongodb-dev.dso86s.xyz
-    type    = "A"
-    ttl     = 1
-    records = [aws_instance.mongodb.private_ip]
-    allow_overwrite = true
-  }
+  # resource "aws_route53_record" "mongodb" {
+  #   zone_id = data.aws_route53_zone.main.zone_id
+  #   name    = "mongodb-${var.environment}.${var.domain_name}" # mongodb-dev.dso86s.xyz
+  #   type    = "A"
+  #   ttl     = 1
+  #   records = [aws_instance.mongodb.private_ip]
+  #   allow_overwrite = true
+  # }
 
 
-  resource "aws_route53_record" "redis" {
-    zone_id = data.aws_route53_zone.main.zone_id
-    name    = "redis-${var.environment}.${var.domain_name}" # redis-dev.dso86s.xyz
-    type    = "A"
-    ttl     = 1
-    records = [aws_instance.redis.private_ip]
-    allow_overwrite = true
-  }
+  # resource "aws_route53_record" "redis" {
+  #   zone_id = data.aws_route53_zone.main.zone_id
+  #   name    = "redis-${var.environment}.${var.domain_name}" # redis-dev.dso86s.xyz
+  #   type    = "A"
+  #   ttl     = 1
+  #   records = [aws_instance.redis.private_ip]
+  #   allow_overwrite = true
+  # }
 
-  resource "aws_route53_record" "rabbitmq" {
-    zone_id = data.aws_route53_zone.main.zone_id
-    name    = "rabbitmq-${var.environment}.${var.domain_name}" # rabbitmq-dev.dso86s.xyz
-    type    = "A"
-    ttl     = 1
-    records = [aws_instance.rabbitmq.private_ip]
-    allow_overwrite = true
-  }
+  # resource "aws_route53_record" "rabbitmq" {
+  #   zone_id = data.aws_route53_zone.main.zone_id
+  #   name    = "rabbitmq-${var.environment}.${var.domain_name}" # rabbitmq-dev.dso86s.xyz
+  #   type    = "A"
+  #   ttl     = 1
+  #   records = [aws_instance.rabbitmq.private_ip]
+  #   allow_overwrite = true
+  # }
 
-  resource "aws_route53_record" "mysql" {
-    zone_id = data.aws_route53_zone.main.zone_id
-    name    = "mysql-${var.environment}.${var.domain_name}" # mysql-dev.dso86s.xyz
-    type    = "A"
-    ttl     = 1
-    records = [aws_instance.mysql.private_ip]
-    allow_overwrite = true
-  }
+  # resource "aws_route53_record" "mysql" {
+  #   zone_id = data.aws_route53_zone.main.zone_id
+  #   name    = "mysql-${var.environment}.${var.domain_name}" # mysql-dev.dso86s.xyz
+  #   type    = "A"
+  #   ttl     = 1
+  #   records = [aws_instance.mysql.private_ip]
+  #   allow_overwrite = true
+  # }
