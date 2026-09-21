adacaf3c37e93d05d9e5a92299420a31a2d309bb
SKR-2021 <karunakar2000@gmail.com>
2026-09-21 17:08:03 +0200

    catalogue

--- dotmart_dev_infra/60-catalogue/main.tf
+++ dotmart_dev_infra/60-catalogue/main.tf
@@ -43,22 +43,22 @@ resource "terraform_data" "catalogue" {
 }
 
 # stop the instance to take image (AMI)
-resource "aws_ec2_instance_state" "catalogue" {
-  instance_id = aws_instance.catalogue.id
-  state       = "stopped"
-  depends_on  = [terraform_data.catalogue]
-}
+# resource "aws_ec2_instance_state" "catalogue" {
+#   instance_id = aws_instance.catalogue.id
+#   state       = "stopped"
+#   depends_on  = [terraform_data.catalogue]
+# }
 
-resource "aws_ami_from_instance" "example" {
-  name               = "dotmart_catalogue_AMI"
-  source_instance_id = aws_instance.catalogue.id
-  depends_on         = [aws_ec2_instance_state.catalogue]
-  
-  tags = merge(
-    local.common_tags,
-    {
-      Name = "${var.project_name}-${var.environment}-catalogue-vm" # dotmart-dev-catalogue-vm
-    }
-  )
+# resource "aws_ami_from_instance" "example" {
+#   name               = "dotmart_catalogue_AMI"
+#   source_instance_id = aws_instance.catalogue.id
+#   depends_on         = [aws_ec2_instance_state.catalogue]
 
-}
+#   tags = merge(
+#     local.common_tags,
+#     {
+#       Name = "${var.project_name}-${var.environment}-catalogue-vm" # dotmart-dev-catalogue-vm
+#     }
+#   )
+
+# }
