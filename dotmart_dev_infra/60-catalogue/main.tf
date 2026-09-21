ccfb819c5a67f5cfdf467c17422358ff018f120c
SKR-2021 <karunakar2000@gmail.com>
2026-09-21 15:51:00 +0200

    catalogue

--- dotmart_dev_infra/60-catalogue/main.tf
+++ dotmart_dev_infra/60-catalogue/main.tf
@@ -46,11 +46,19 @@ resource "terraform_data" "catalogue" {
 resource "aws_ec2_instance_state" "catalogue" {
   instance_id = aws_instance.catalogue.id
   state       = "stopped"
-  depends_on  = ["terraform_data.catalogue"]
+  depends_on  = [terraform_data.catalogue]
 }
 
 resource "aws_ami_from_instance" "example" {
   name               = "dotmart_catalogue_AMI"
   source_instance_id = aws_instance.catalogue.id
-  depends_on         = ["aws_ec2_instance_state.catalogue"]
+  depends_on         = [aws_ec2_instance_state.catalogue]
+  
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project_name}-${var.environment}-catalogue-vm" # dotmart-dev-catalogue-vm
+    }
+  )
+
 }
