6988be8896b802fcc656de3b65bf590ebddcd233
SKR-2021 <karunakar2000@gmail.com>
2026-09-22 23:30:20 +0200

    catalogue

--- dotmart_dev_infra/20-bastion/main.tf
+++ dotmart_dev_infra/20-bastion/main.tf
@@ -5,6 +5,11 @@ resource "aws_instance" "bastion" {
   subnet_id = local.public_subnet_ids
   iam_instance_profile = aws_iam_instance_profile.bastion.name
 
+  root_block_device {
+    volume_size = 30
+    volume_type = "gp3"
+}
+
   user_data = file("bastion.sh")
   tags = merge(
     local.common_tags,
