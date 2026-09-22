247c6053f2a280459507d43d790c83667d647327
SKR-2021 <karunakar2000@gmail.com>
2026-09-22 18:51:52 +0200

    catalogue

--- dotmart_dev_infra/60-catalogue/locals.tf
+++ dotmart_dev_infra/60-catalogue/locals.tf
@@ -1,7 +1,9 @@
 locals {
+  common_name_suffix = "${var.project_name}-${var.environment}" # dotmart-dev
   ami_id             = data.aws_ami.dotmart.id
   catalogue_sg_id    = data.aws_ssm_parameter.catalogue_sg_id.value
   private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
+  vpc_id             = data.aws_ssm_parameter.vpc_id.value
 
   common_tags = {
     Project     = var.project_name
--- dotmart_dev_infra/60-catalogue/main.tf
+++ dotmart_dev_infra/60-catalogue/main.tf
@@ -42,23 +42,115 @@ resource "terraform_data" "catalogue" {
   }
 }
 
-# stop the instance to take image (AMI)
-# resource "aws_ec2_instance_state" "catalogue" {
-#   instance_id = aws_instance.catalogue.id
-#   state       = "stopped"
-#   depends_on  = [terraform_data.catalogue]
-# }
+# Stop the instance to take image (AMI)
+resource "aws_ec2_instance_state" "catalogue" {
+  instance_id = aws_instance.catalogue.id
+  state       = "stopped"
+  depends_on  = [terraform_data.catalogue]
+}
 
-# resource "aws_ami_from_instance" "example" {
-#   name               = "dotmart_catalogue_AMI"
-#   source_instance_id = aws_instance.catalogue.id
-#   depends_on         = [aws_ec2_instance_state.catalogue]
+resource "aws_ami_from_instance" "catalogue" {
+  name               = "${local.common_name_suffix}-catalogue-ami"
+  source_instance_id = aws_instance.catalogue.id
+  depends_on         = [aws_ec2_instance_state.catalogue]
 
-#   tags = merge(
-#     local.common_tags,
-#     {
-#       Name = "${var.project_name}-${var.environment}-catalogue-vm" # dotmart-dev-catalogue-vm
-#     }
-#   )
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project_name}-${var.environment}-catalogue-ami" # dotmart-dev-catalogue-vm
+    }
+  )
+}
 
-# }
+resource "aws_lb_target_group" "catalogue" {
+  name     = "${local.common_name_suffix}-catalogue-ami"
+  port     = 8080
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+
+  health_check {
+    healthy_threshold   = 2
+    interval            = 2
+    matcher             = 200 - 299
+    path                = "/health"
+    port                = 8080
+    protocol            = "HTTP"
+    timeout             = 2
+    unhealthy_threshold = 2
+
+  }
+}
+
+resource "aws_launch_template" "catalogue" {
+  name     = "${local.common_name_suffix}-catalogue-template"
+  image_id = "aws_ami_from_instance.catalogue.id"
+
+  instance_initiated_shutdown_behavior = "terminate"
+  instance_type                        = "t3.micro"
+
+  vpc_security_group_ids = [local.catalogue_sg_id]
+
+  # Tags attached to the instance
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = merge(
+      local.common_tags,
+      {
+        Name : "${local.common_name_suffix}-catalogue-template"
+      }
+    )
+  }
+  # Tags attached to the volume created by instance
+  tag_specifications {
+    resource_type = "volume"
+
+    tags = merge(
+      local.common_tags,
+      {
+        Name : "${local.common_name_suffix}-catalogue-template"
+      }
+    )
+  }
+  # Tags attached to the lauch template
+  tags = merge(
+    local.common_tags,
+    {
+      Name : "${local.common_name_suffix}-catalogue-template"
+    }
+  )
+}
+
+resource "aws_autoscaling_group" "catalogue" {
+  name                      = "${local.common_name_suffix}-catalogue-asg"
+  max_size                  = 10
+  min_size                  = 1
+  health_check_grace_period = 100
+  health_check_type         = "ELB"
+  desired_capacity          = 1
+  force_delete              = false
+  launch_template {
+    id      = aws_launch_template.catalogue.id
+    version = aws_launch_template.catalogue.latest_version
+  }
+  vpc_zone_identifier = local.private_subnet_ids
+
+  dynamic "tag" {
+    for_each = merge(
+      local.common_tags,
+      {
+        Name = "${local.common_name_suffix}-catalogue"
+      }
+    )
+
+    content {
+      key                 = "tag.key"
+      value               = "tag.value"
+      propagate_at_launch = true
+    }
+  }
+  timeouts {
+    delete = "15m"
+  }
+}
+
