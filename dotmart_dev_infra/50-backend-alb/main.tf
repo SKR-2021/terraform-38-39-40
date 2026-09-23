21ee41b32b87a8ed41b6a7e267be42b2bdc907c6
SKR-2021 <karunakar2000@gmail.com>
2026-09-23 16:26:25 +0200

    catalogue-lb

--- dotmart_dev_infra/50-backend-alb/main.tf
+++ dotmart_dev_infra/50-backend-alb/main.tf
@@ -17,7 +17,7 @@ resource "aws_lb" "backend_alb" {
 }
 
 # Backend ALB listing on port number 80
-resource "aws_lb_listener" "front_end" {
+resource "aws_lb_listener" "backend_alb" {
   load_balancer_arn = aws_lb.backend_alb.arn
   port              = "80"
   protocol          = "HTTP"
@@ -30,5 +30,18 @@ resource "aws_lb_listener" "front_end" {
       message_body = "This is from Backend ALB HTTP"
       status_code  = "200"
     }
+  }
+}
+
+resource "aws_route53_record" "backend_alb" {
+  zone_id = data.aws_route53_zone.main.id
+  name    = "*.backend-alb-${{var.environment}}.${{var.domain_name}}"
+  type    = "A"
+
+  alias {
+    # These are ALB details, not our domain details 
+    name                   = aws_lb.backend_alb.dns_name
+    zone_id                = aws_lb.backend_alb.zone_id
+    evaluate_target_health = true
   }
 }
