1dcbc85fba6c8964954088160f6dce497aa35933
SKR-2021 <karunakar2000@gmail.com>
2026-09-23 16:34:35 +0200

    catalogue-lb

--- dotmart_dev_infra/60-catalogue/locals.tf
+++ dotmart_dev_infra/60-catalogue/locals.tf
@@ -4,7 +4,7 @@ locals {
   catalogue_sg_id    = data.aws_ssm_parameter.catalogue_sg_id.value
   private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
   vpc_id             = data.aws_ssm_parameter.vpc_id.value
-  backend_alb_sg_id = data.aws_ssm_parameter.backend_alb_sg_id.value
+ 
 
   common_tags = {
     Project     = var.project_name
